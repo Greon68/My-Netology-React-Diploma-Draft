a0b778ed9549374bf90b1de4b06bbb7afa0fc091
Greon68 <greon-ya@yandex.ru>
2026-04-08 17:39:17 +0300

    commit-9

--- src/components/Catalog/SearchCatalog.jsx
+++ src/components/Catalog/SearchCatalog.jsx
@@ -1,130 +1,158 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "../../config/api";
 import { CatalogPreview } from "../Home/CatalogPreview";
 import { CatalogMenu } from "../Home/CatalogMenu";
 import { ButtonLoadeMore } from "../Home/ButtonLoadeMore";
+import { useLocation } from "react-router";
+
+import "./style.scss";
 
 // Количество загружаемых товаров по клику по кнопке "Загрузить ещё":
-const countLoadeMoreGoods = 6 ;
+const countLoadeMoreGoods = 6;
 
-export const SearchCatalog= ({categories, goodsAll})=> {
-    // console.log( "SearchCatalog props - ",props) 
+export const SearchCatalog = ({ categories, goodsAll }) => {
+  // console.log( "SearchCatalog props - ",props)
 
-    // Данные в поле поиска формы
-    const[search, setSearch]= useState({
-        title:''
-    });
+  // Данные в поле поиска формы
+  const [search, setSearch] = useState({
+    title: "",
+  });
 
-    // список товаров для отрисовки:
-    const[ previewList , setPreviewList] = useState(goodsAll) ;
-    // выбранная категория товаров:  
-    const[ selected , setSelected] = useState({id:1, title:'Все'});
+  // ***************************************
+  // Объект location из  useLocation()
+  let location = useLocation();
+  console.log("Catalog location - ", location);
 
-    // Добавляем категорию "Все" в массив категорий
-    if(categories.length <= 4){
-        categories.unshift({id:1 , title:"Все"}); 
-    };
+  // Если получены данные из поля поиска главного меню,
+  // запишем их в search.title:
+  useEffect(() => {
+    // console.log("SearchCatalog location.state - ", location.state);
+    if (location.state.valueSearch) {
+      setSearch({ title: location.state.valueSearch });
+    }
+  }, [location]);
 
-    // Данные для показа/скрытия кнопки "Загрузить ещё".
-    // Сохраняем  в состоянии длину массива подгружаемых данных - countCurrentGoods:
-    const [countCurrentGoods , setCountCurrentGoods]= useState(countLoadeMoreGoods);
-   
-    // Cвязь с кнопками выбора категории товара:
-    // На вход получаем выбранную кнопку(объект selectButton) и массив данных (data)
-    // для выбранной категории товаров:
-    const onSelectFilter = ( selectButton , data )=> {
-        // При нажатии на кнопку меню:       
-        console.log ("HomeCatalog : получен фильтр - ", selectButton);      
-        console.log ("HomeCatalog : получены данные :  ", data)
-         // 1. Фиксируем выбранный объект категории в константу selected:
-        setSelected(selectButton);
-          // 2. Фиксируем объект со списком товаров по выбранной категории в previewList ;       
-        setPreviewList(data) ; 
+  // список товаров для отрисовки:
+  const [previewList, setPreviewList] = useState(goodsAll);
+  // выбранная категория товаров:
+  const [selected, setSelected] = useState({ id: 1, title: "Все" });
 
-        // 3. Обновляем состояние для кнопки "Загрузить ещё"
-        setCountCurrentGoods(countLoadeMoreGoods)
+  // Добавляем категорию "Все" в массив категорий
+  if (categories.length <= 4) {
+    categories.unshift({ id: 1, title: "Все" });
+  }
+
+  // Данные для показа/скрытия кнопки "Загрузить ещё".
+  // Сохраняем  в состоянии длину массива подгружаемых данных - countCurrentGoods:
+  const [countCurrentGoods, setCountCurrentGoods] =
+    useState(countLoadeMoreGoods);
+
+  // Cвязь с кнопками выбора категории товара:
+  // На вход получаем выбранную кнопку(объект selectButton) и массив данных (data)
+  // для выбранной категории товаров:
+  const onSelectFilter = (selectButton, data) => {
+    // При нажатии на кнопку меню:
+    console.log("HomeCatalog : получен фильтр - ", selectButton);
+    console.log("HomeCatalog : получены данные :  ", data);
+    // 1. Фиксируем выбранный объект категории в константу selected:
+    setSelected(selectButton);
+    // 2. Фиксируем объект со списком товаров по выбранной категории в previewList ;
+    setPreviewList(data);
+
+    // 3. Обновляем состояние для кнопки "Загрузить ещё"
+    setCountCurrentGoods(countLoadeMoreGoods);
+  };
+
+  //  Загрузка данных по нажатию кнопки "Загрузить ещё" :
+  const loadeMoreData = (data) => {
+    console.log(" Догрузка данных в функцию loadeMoreData -", data);
+    setPreviewList([...previewList, ...data]);
+
+    setCountCurrentGoods(data.length);
+  };
+
+  // Функция загрузки данных по содержимому поля поиска search для полного списка:
+  // http://localhost:7070/api/items?q=...
+  const onFetchSearchAll = async () => {
+    const resp = await fetch(`${BASE_URL}/api/items?q=${search.title} `);
+    const data = await resp.json();
+    setPreviewList(data);
+  };
+
+  // Функция загрузки данных по содержимому поля поиска search для категорий:
+  // const url = BASE_URL + `/api/items?categoryId=${categoryId}&offset=${nextOffset}`;
+  // http://localhost:7070/api/items?q=...
+  const onFetchSearch = async (categoryId) => {
+    const resp = await fetch(
+      `${BASE_URL}/api/items?categoryId=${categoryId}&q=${search.title} `
+    );
+    const data = await resp.json();
+    setPreviewList(data);
+  };
+
+  // oбработчик onChange для поля поиска :
+  const onSearchChange = (e) => {
+    const { name, value } = e.target;
+    // console.log ("onSearchChange name -" , name , " value -", value );
+    setSearch((prev) => ({ ...prev, [name]: value }));
+  };
+
+  /* Обработчик события onSubmit у формы с полем поиска  */
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log("SearchCatalog form target -", e.target);
+
+    if (selected.id === 1) {
+      onFetchSearchAll();
+    } else {
+      onFetchSearch(selected.id);
     }
 
-    //  Загрузка данных по нажатию кнопки "Загрузить ещё" :
-    const loadeMoreData = (data)=> {
-        console.log( " Догрузка данных в функцию loadeMoreData -", data);
-        setPreviewList([ ...previewList, ...data]);
-
-        setCountCurrentGoods(data.length)
+    if (location.state.valueSearch) {
+      location.state.valueSearch = "";
     }
+  };
 
+  console.log("SearchCatalog search -", search.title);
 
+  return (
+    <div className="catalog">
+      <h2 className="text-center title-block">Каталог</h2>
+      <form className="catalog-search-form" onSubmit={onSearchSubmit}>
+        <input
+          type="search"
+          className="form-control"
+          name="title"
+          value={search.title}
+          onChange={onSearchChange}
+          placeholder="Поиск"
+        />
+        {
+          search.title &&   
+            <button type="submit" className="button-catalog-search-form">
+              Найти
+            </button>
+        }
+      
+      </form>
 
-    // Функция загрузки данных по содержимому поля поиска search для полного списка:
-    // http://localhost:7070/api/items?q=...
-    const onFetchSearchAll = async ()=>{
-        const resp = await fetch(`${BASE_URL}/api/items?q=${search.title} ` );
-        const data = await resp.json();
-        setPreviewList(data)
-        
-     }
+      <CatalogMenu
+        categories={categories}
+        selected={selected}
+        onSelectFilter={onSelectFilter}
+      />
 
-    // Функция загрузки данных по содержимому поля поиска search для категорий:
-    // const url = BASE_URL + `/api/items?categoryId=${categoryId}&offset=${nextOffset}`;
-    // http://localhost:7070/api/items?q=...
-    const onFetchSearch = async (categoryId)=>{
-        const resp = await fetch(`${BASE_URL}/api/items?categoryId=${categoryId}&q=${search.title} ` );
-        const data = await resp.json();
-        setPreviewList(data)
-        
-     }
+      <CatalogPreview previewList={previewList} />
 
-    // oбработчик onChange для поля поиска :
-    const onSearchChange = (e)=> {
-        const { name , value} = e.target ;
-        // console.log ("onSearchChange name -" , name , " value -", value );
-        setSearch( prev => ({ ...prev, [name]:value }))
-        if(selected.id===1){
-                 // обработка с задержкой
-        setTimeout(()=> onFetchSearchAll(), 1500 )
-        } else {
-            setTimeout(()=> onFetchSearch(selected.id), 1500 )
-        }    
-    }
-
-    console.log("SearchCatalog search -", search.title)
-
-    return (
-        <div className="catalog">
-            <h2 className="text-center title-block">Каталог</h2>
-
-            <form className="catalog-search-form form-inline">
-            <input 
-                type="search"
-                className="form-control"
-                name="title"
-                value={search.title}
-                onChange={onSearchChange}
-                placeholder="Поиск"
-                
-                />
-            </form>
-
-            <CatalogMenu  
-                categories={ categories}
-                selected={selected}
-                onSelectFilter={onSelectFilter}                   
-            />     
-     
-            <CatalogPreview previewList={previewList}/>
-
-            <div className="text-center">
-                <ButtonLoadeMore
-                    selected={selected}
-                    loadeMoreData={loadeMoreData}
-                    lengthPreviewList={previewList.length}
-                    countLoadeMoreGoods={countLoadeMoreGoods}
-                    countCurrentGoods={countCurrentGoods}
-                />
-            </div>   
-
-        </div>
-    )
-}
-
-
+      <div className="text-center">
+        <ButtonLoadeMore
+          selected={selected}
+          loadeMoreData={loadeMoreData}
+          lengthPreviewList={previewList.length}
+          countLoadeMoreGoods={countLoadeMoreGoods}
+          countCurrentGoods={countCurrentGoods}
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/Main/MainMenuRight.jsx
+++ src/components/Main/MainMenuRight.jsx
@@ -1,73 +1,50 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router";
-import { CART_ROUT } from "../../router/routes";
+import { CART_ROUT, CATALOG_ROUT } from "../../router/routes";
 import { CountOrders } from "./CountOrders";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { useNavigate } from "react-router";
 
-// export const MainMenuRight = () => {
-//   return (
-//     <div className="main-menu-right">
-//       <img src="../../../img/header-controls-sprite.png" alt="" />
-//       <div className="header-controls-pics">
-//         ЛУПА
-//         <div
-//           data-id="search-expander"
-//           className="header-controls-pic header-controls-search"
-//           onClick={() => {
-//             console.log("Клик по лупе");
-//           }}
-//         ></div>
-
-//         КОРЗИНА
-//         <Link
-//           className="header-controls-pic header-controls-cart"
-//           to={CART_ROUT}
-//           onClick={() => console.log("Клик по корзине")}
-//         >
-//           <CountOrders />
-         
-//         </Link>
-//       </div>
-//       ПОЛЕ ПОИСКА - Форма
-//       <form
-//         data-id="search-form"
-//         className="header-controls-search-form form-inline invisible"
-//       >
-//         <input
-//           className="form-control"
-//           id="search-field"
-//           name="search-field"
-//           placeholder="Поиск"
-//         />
-//       </form>
-//     </div>
-//   );
-// };
-
-// Вношу изменения:
 export const MainMenuRight = () => {
-  return (
-    <div className="main-menu-right">
-      
-      {/* ПОЛЕ ПОИСКА - Форма */}
-      <form
-        data-id="search-form"
-        className="header-controls-search-form form-inline invisible"
-      >
-        <input
-          className="form-control"
-          id="search-field"
-          name="search-field"
-          placeholder="Поиск"
-        />
-      </form>
+  // Показ\скрытие поля поиска:
+  const [active, setActive] = useState(false);
+  // Состояние формы поиска:
+  const [form, setForm] = useState({
+    searchField: "",
+  });
 
+  // Навигация:
+  let navigate = useNavigate()
+
+  const onFormChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSearchClick = ()=> {
+    // console.log("Клик по лупе")
+    setActive(prev =>!prev);
+    if(active && form.searchField){
+      console.log("form -", form)
+      navigate(
+        CATALOG_ROUT,
+        {state:{valueSearch :form.searchField }}
+      );
+      setForm({
+        searchField: "",
+      })
+    }
+  }
+
+  return (
+    /* Контейнер  правой части главного меню */
+    <div className="main-menu-right">
       <div className="header-controls-pics">
-        {/* ЛУПА */}
+        {/* ЛУПА: useNavigate() */}
         <div
           data-id="search-expander"
           className="header-controls-pic header-controls-search"
-          onClick={() => {
-            console.log("Клик по лупе");
-          }}
+          onClick={onSearchClick}
         ></div>
 
         {/* КОРЗИНА */}
@@ -75,10 +52,27 @@ export const MainMenuRight = () => {
           className="header-controls-pic header-controls-cart"
           to={CART_ROUT}
           onClick={() => console.log("Клик по корзине")}
+          state={{data:"Привет, корзина !!!"}}
         >
           <CountOrders />
         </Link>
       </div>
+      {/* ПОЛЕ ПОИСКА - Форма */}
+      <form
+        data-id="search-form"
+        // className="header-controls-search-form form-inline invisible"
+        className="header-controls-search-form form-inline "
+      >
+        <input
+          // type="search"
+          className={active ? "form-control" : "form-control invisible"}
+          id="search-field"
+          name="searchField"
+          value={form.searchField}
+          onChange={onFormChange}
+          placeholder="Поиск"
+        />
+      </form>
     </div>
   );
 };
